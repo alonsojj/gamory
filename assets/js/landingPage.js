1579cd75816378a2cd8ec61e3f4c5b31875cf74b
alonsojj <juan13032007@gmail.com>
2025-07-07 16:19:58 -0300

    minorfixes

--- assets/js/landingPage.js
+++ assets/js/landingPage.js
@@ -2,31 +2,30 @@
 const texts = ["Explore", "Descubra", "Jogue", "Conquiste"];
 let count = 0;
 let index = 0;
-let currentText = '';
-let letter = '';
+let currentText = "";
+let letter = "";
 
-(function type(){
-    if (count === texts.length) {
-        count = 0;
-    }
-    currentText = texts[count];
-    letter = currentText.slice(0, ++index);
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
 
-    document.querySelector('.typing').textContent = letter;
-    if (letter.length === currentText.length) {
-        count++;
-        index = 0;
-        setTimeout(type, 1000); // espera depois da palavra
-    } else {
-        setTimeout(type, 150); // velocidade da digitação
-    }
-}());
+  document.querySelector(".typing").textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 1000);
+  } else {
+    setTimeout(type, 150);
+  }
+})();
 
-// Abrir telas de Login e Cadastro
-document.getElementById('loginBtn').addEventListener('click', function() {
-    window.location.href = "login.html"; // Troca pelo seu arquivo real
+document.getElementById("loginBtn").addEventListener("click", function () {
+  window.location.href = "login.html";
 });
 
-document.getElementById('cadastroBtn').addEventListener('click', function() {
-    window.location.href = "cadastro.html"; // Troca pelo seu arquivo real
+document.getElementById("cadastroBtn").addEventListener("click", function () {
+  window.location.href = "cadastro.html";
 });
